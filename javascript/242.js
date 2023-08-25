4cd7d2212498d63584b3a20fda4972075d64142c
John Firebaugh <j.s.firebaugh@gmail.com>
2023-08-25 18:34:16 -0400

    Create 242.js

---
+++ javascript/242.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    return s.split("").sort().join("") == t.split("").sort().join("")
+};
+
+console.log( isAnagram("anagram", "nagaram"))
+console.log( isAnagram("rat", "car"))
