f855d150f313a65c24be6a06eb938bb6209273e2
John Firebaugh <j.s.firebaugh@gmail.com>
2023-10-18 14:59:31 -0400

    Create 66.kt

---
+++ kotlin/66.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+
+        for ( i in digits.lastIndex downTo 0 ){
+            if ( digits[i] != 9 ){
+                digits[i] = digits[i] + 1
+                return digits
+            }
+
+            digits[i] = 0
+        }
+        
+        val returnArray = IntArray( digits.size + 1 )
+        returnArray[0] = 1
+        return returnArray
+    }
+}
+
+fun main() {
+    val input1 = intArrayOf(1,2,3)
+    val input2 = intArrayOf(4,3,2,1)
+    val input3 = intArrayOf(9)
+    val input4 = intArrayOf(1,9)
+
+    val solution = Solution()
+    println( solution.plusOne(input1).joinToString(",") )
+    println( solution.plusOne(input2).joinToString(",") )
+    println( solution.plusOne(input3).joinToString(",") )
+    println( solution.plusOne(input4).joinToString(",") )
+}
