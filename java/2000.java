ecd27ff26a27e3436ec10026f43806f7fe5f1833
John Firebaugh <j.s.firebaugh@gmail.com>
2024-01-09 17:19:59 -0500

    Create 2000.java

---
+++ java/2000.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+
+        if(word.indexOf(ch) == -1){
+            return word;
+        }
+
+        StringBuilder sb = new StringBuilder(word.substring(0, word.indexOf(ch) + 1));
+        
+        return sb
+                .reverse()
+                .append(word.substring(word.indexOf(ch) + 1))
+                .toString();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.reversePrefix("abcdefd", 'd'));
+
+
+    }
+}
